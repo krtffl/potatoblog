0d4d064519f8765d26b0ddffb4ada9a0a67e0acb
krtffl <didac.pereze@gmail.com>
2024-09-04 16:44:29 +0200

    feat: add post, tag models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+
+
+class Post(models.Model):
+    STATUS = (
+        (0, "Draft"),
+        (1, "Published"),
+    )
+
+    title = models.CharField(max_length=255, unique=True)
+    # related_name="posts" defines the reverse relation
+    tags = models.ManyToManyField("Tag", related_name="posts")
+    # auto_now_add=True automatically updates the value to current date time
+    # when the object is first created
+    created_on = models.DateTimeField(auto_now_add=True)
+    # auto_now=True automatically updates the value to current date time
+    # whenever the object is saved
+    last_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    # metadata
+    class Meta:
+        ordering = ["-created_on"]
+
+    # default human-readable representation
+    def __str__(self):
+        return self.title
